bb55b2265ff23b9ac8c5de4e12258b7abfc4427e
kmcnee <kyle.mcnee@gmail.com>
2016-03-02 13:15:11 -0500

    Added StockQuote class, buttons, and API calls coded as strings

--- SyncAdaptersLab/app/src/main/java/com/charlesdrews/syncadapterslab/MainActivity.java
+++ SyncAdaptersLab/app/src/main/java/com/charlesdrews/syncadapterslab/MainActivity.java
@@ -2,12 +2,48 @@ package com.charlesdrews.syncadapterslab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private String FACEBOOK = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=fb";
+    private String DISNEY = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=dis";
+    private String MARATHON_OIL = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=mro";
+    private String MONSANTO = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=mon";
+    private String BOFA = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=bac";
+
+    Button mSyncNowButton;
+    Button mOneMinuteButton;
+    Button mFiveMinuteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSyncNowButton = (Button) findViewById(R.id.manualButton);
+        mOneMinuteButton = (Button) findViewById(R.id.minuteButton);
+        mFiveMinuteButton = (Button) findViewById(R.id.fiveMinuteButton);
+
+        mSyncNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mOneMinuteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mFiveMinuteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
